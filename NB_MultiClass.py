dd75b78b35c8fdc7c78680fa46775ff55d1ff73c
Junha Park <junha.park@mail.mcgill.ca>
2019-10-10 22:19:14 -0400

    fixes with naive bayes model fit() and predict() methods

--- NB_MultiClass.py
+++ NB_MultiClass.py
@@ -3,172 +3,143 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 class naiveBayes:
-    animeTS = None
-    AskRedditTS = None
-    baseballTS = None
-    canadaTS = None
-    conspiracyTS = None
-    europeTS = None
-    funnyTS = None
-    gameofthronesTS = None
-    GlobalOffensiveTS = None
-    hockeyTS = None
-    leagueoflegnedsTS = None
-    moviesTS = None
-    MusicTS = None
-    nbaTS = None
-    nflTS = None
-    OverwatchTS = None
-    soccerTS = None
-    treesTS = None
-    worldnewsTS = None
-    wowTS = None
 
-    countTS = None
+    thetaJY = {}
+    thetaY = []
 
-    def __init__(self, trainSet):
-        self.countTS = trainSet.shape[0] # number of training examples
+    def __init__(self):
+        pass
 
-        self.setTS(trainSet)
+    def fit(self, trainSet):
 
-    def setTS(self, trainSet):
-        self.animeTS = trainSet[np.where(trainSet[:, -1] == "anime")]
-        self.AskRedditTS = trainSet[np.where(trainSet[:, -1] == "AskReddit")]
-        self.baseballTS = trainSet[np.where(trainSet[:, -1] == "baseball")]
-        self.canadaTS = trainSet[np.where(trainSet[:, -1] == "canada")]
-        self.conspiracyTS = trainSet[np.where(trainSet[:, -1] == "conspiracy")]
-        self.europeTS = trainSet[np.where(trainSet[:, -1] == "europe")]
-        self.funnyTS = trainSet[np.where(trainSet[:, -1] == "funny")]
-        self.gameofthronesTS = trainSet[np.where(trainSet[:, -1] == "gameofthrones")]
-        self.GlobalOffensiveTS = trainSet[np.where(trainSet[:, -1] == "GlobalOffensive")]
-        self.hockeyTS = trainSet[np.where(trainSet[:, -1] == "hockey")]
-        self.leagueoflegnedsTS = trainSet[np.where(trainSet[:, -1] == "leagueoflegends")]
-        self.moviesTS = trainSet[np.where(trainSet[:, -1] == "movies")]
-        self.MusicTS = trainSet[np.where(trainSet[:, -1] == "Music")]
-        self.nbaTS = trainSet[np.where(trainSet[:, -1] == "nba")]
-        self.nflTS = trainSet[np.where(trainSet[:, -1] == "nfl")]
-        self.OverwatchTS = trainSet[np.where(trainSet[:, -1] == "Overwatch")]
-        self.soccerTS = trainSet[np.where(trainSet[:, -1] == "soccer")]
-        self.treesTS = trainSet[np.where(trainSet[:, -1] == "trees")]
-        self.worldnewsTS = trainSet[np.where(trainSet[:, -1] == "worldnews")]
-        self.wowTS = trainSet[np.where(trainSet[:, -1] == "wow")]
+        countTS = trainSet.shape[0]  # number of training examples
+
+        animeTS = trainSet[np.where(trainSet[:, -1] == "anime")]
+        askRedditTS = trainSet[np.where(trainSet[:, -1] == "AskReddit")]
+        baseballTS = trainSet[np.where(trainSet[:, -1] == "baseball")]
+        canadaTS = trainSet[np.where(trainSet[:, -1] == "canada")]
+        conspiracyTS = trainSet[np.where(trainSet[:, -1] == "conspiracy")]
+        europeTS = trainSet[np.where(trainSet[:, -1] == "europe")]
+        funnyTS = trainSet[np.where(trainSet[:, -1] == "funny")]
+        gameofthronesTS = trainSet[np.where(trainSet[:, -1] == "gameofthrones")]
+        globalOffensiveTS = trainSet[np.where(trainSet[:, -1] == "GlobalOffensive")]
+        hockeyTS = trainSet[np.where(trainSet[:, -1] == "hockey")]
+        leagueoflegnedsTS = trainSet[np.where(trainSet[:, -1] == "leagueoflegends")]
+        moviesTS = trainSet[np.where(trainSet[:, -1] == "movies")]
+        musicTS = trainSet[np.where(trainSet[:, -1] == "Music")]
+        nbaTS = trainSet[np.where(trainSet[:, -1] == "nba")]
+        nflTS = trainSet[np.where(trainSet[:, -1] == "nfl")]
+        overwatchTS = trainSet[np.where(trainSet[:, -1] == "Overwatch")]
+        soccerTS = trainSet[np.where(trainSet[:, -1] == "soccer")]
+        treesTS = trainSet[np.where(trainSet[:, -1] == "trees")]
+        worldnewsTS = trainSet[np.where(trainSet[:, -1] == "worldnews")]
+        wowTS = trainSet[np.where(trainSet[:, -1] == "wow")]
+
+        self.thetaY[0] = animeTS.shape[0] / countTS
+        self.thetaY[1] = askRedditTS.shape[0] / countTS
+        self.thetaY[2] = baseballTS.shape[0] / countTS
+        self.thetaY[3] = canadaTS.shape[0] / countTS
+        self.thetaY[4] = conspiracyTS.shape[0] / countTS
+        self.thetaY[5] = europeTS.shape[0] / countTS
+        self.thetaY[6] = funnyTS.shape[0] / countTS
+        self.thetaY[7] = gameofthronesTS.shape[0] / countTS
+        self.thetaY[8] = globalOffensiveTS.shape[0] / countTS
+        self.thetaY[9] = hockeyTS.shape[0] / countTS
+        self.thetaY[10] = leagueoflegnedsTS.shape[0] / countTS
+        self.thetaY[11] = moviesTS.shape[0] / countTS
+        self.thetaY[12] = musicTS.shape[0] / countTS
+        self.thetaY[13] = nbaTS.shape[0] / countTS
+        self.thetaY[14] = nflTS.shape[0] / countTS
+        self.thetaY[15] = overwatchTS.shape[0] / countTS
+        self.thetaY[16] = soccerTS.shape[0] / countTS
+        self.thetaY[17] = treesTS.shape[0] / countTS
+        self.thetaY[18] = worldnewsTS.shape[0] / countTS
+        self.thetaY[19] = wowTS.shape[0] / countTS
+
+        numCols = trainSet.shape[1] -1
+
+        for j in range(numCols):
+            self.thetaJY[j] = {}
+
+            set = animeTS[np.where(animeTS[:, j] == 1)]
+            self.thetaJY[j][0] = (set.shape[0] + 1) / (animeTS.shape[0] + 2)
+
+            set = askRedditTS[np.where(askRedditTS[:, j] == 1)]
+            self.thetaJY[j][1] = (set.shape[0] + 1) / (askRedditTS.shape[0] + 2)
+
+            set = baseballTS[np.where(baseballTS[:, j] == 1)]
+            self.thetaJY[j][2] = (set.shape[0] + 1) / (baseballTS.shape[0] + 2)
+
+            set = canadaTS[np.where(canadaTS[:, j] == 1)]
+            self.thetaJY[j][3] = (set.shape[0] + 1) / (canadaTS.shape[0] + 2)
+
+            set = conspiracyTS[np.where(conspiracyTS[:, j] == 1)]
+            self.thetaJY[j][4] = (set.shape[0] + 1) / (conspiracyTS.shape[0] + 2)
+
+            set = europeTS[np.where(europeTS[:, j] == 1)]
+            self.thetaJY[j][5] = (set.shape[0] + 1) / (europeTS.shape[0] + 2)
+
+            set = funnyTS[np.where(funnyTS[:, j] == 1)]
+            self.thetaJY[j][6] = (set.shape[0] + 1) / (funnyTS.shape[0] + 2)
+
+            set = gameofthronesTS[np.where(gameofthronesTS[:, j] == 1)]
+            self.thetaJY[j][7] = (set.shape[0] + 1) / (gameofthronesTS.shape[0] + 2)
+
+            set = globalOffensiveTS[np.where(globalOffensiveTS[:, j] == 1)]
+            self.thetaJY[j][8] = (set.shape[0] + 1) / (globalOffensiveTS.shape[0] + 2)
+
+            set = hockeyTS[np.where(hockeyTS[:, j] == 1)]
+            self.thetaJY[j][9] = (set.shape[0] + 1) / (hockeyTS.shape[0] + 2)
+
+            set = leagueoflegnedsTS[np.where(leagueoflegnedsTS[:, j] == 1)]
+            self.thetaJY[j][10] = (set.shape[0] + 1) / (leagueoflegnedsTS.shape[0] + 2)
+
+            set = moviesTS[np.where(moviesTS[:, j] == 1)]
+            self.thetaJY[j][11] = (set.shape[0] + 1) / (moviesTS.shape[0] + 2)
+
+            set = musicTS[np.where(musicTS[:, j] == 1)]
+            self.thetaJY[j][12] = (set.shape[0] + 1) / (musicTS.shape[0] + 2)
+
+            set = nbaTS[np.where(nbaTS[:, j] == 1)]
+            self.thetaJY[j][13] = (set.shape[0] + 1) / (nbaTS.shape[0] + 2)
+
+            set = nflTS[np.where(nflTS[:, j] == 1)]
+            self.thetaJY[j][14] = (set.shape[0] + 1) / (nflTS.shape[0] + 2)
+
+            set = overwatchTS[np.where(overwatchTS[:, j] == 1)]
+            self.thetaJY[j][15] = (set.shape[0] + 1) / (overwatchTS.shape[0] + 2)
+
+            set = soccerTS[np.where(soccerTS[:, j] == 1)]
+            self.thetaJY[j][16] = (set.shape[0] + 1) / (soccerTS.shape[0] + 2)
+
+            set = treesTS[np.where(treesTS[:, j] == 1)]
+            self.thetaJY[j][17] = (set.shape[0] + 1) / (treesTS.shape[0] + 2)
+
+            set = worldnewsTS[np.where(askRedditTS[:, j] == 1)]
+            self.thetaJY[j][18] = (set.shape[0] + 1) / (askRedditTS.shape[0] + 2)
+
+            set = wowTS[np.where(wowTS[:, j] == 1)]
+            self.thetaJY[j][19] = (set.shape[0] + 1) / (wowTS.shape[0] + 2)
+
+
 
     def predict(self, testSet):
-        numRows = testSet.shape[0]
-        predictList = []
+        numRows = testSet.shape[0] #number of testset
+        numClasses = 20
+        numFeatures = testSet.shape[1]
+        predictList = [] #collection of prediction class for each test example
         for i in range(numRows):
             x = testSet[i]
-            numCols = x.shape[1] - 1
-            # this is initial p(y=c)
-            animeProb = self.animeTS.shape[0] / self.countTS
-            AskRedditProb = self.AskRedditTS.shape[0] / self.countTS
-            baseballProb = self.baseballTS.shape[0] / self.countTS
-            canadaProb = self.canadaTS.shape[0] / self.countTS
-            conspiracyProb = self.conspiracyTS.shape[0] / self.countTS
-            europeProb = self.europeTS.shape[0] / self.countTS
-            funnyProb = self.funnyTS.shape[0] / self.countTS
-            gameofthronesProb = self.gameofthronesTS.shape[0] / self.countTS
-            GlobalOffensiveProb = self.GlobalOffensiveTS.shape[0] / self.countTS
-            hockeyProb = self.hockeyTS.shape[0] / self.countTS
-            leagueoflegendsProb = self.leagueoflegnedsTS.shape[0] / self.countTS
-            moviesProb = self.moviesTS.shape[0] / self.countTS
-            MusicProb = self.MusicTS.shape[0] / self.countTS
-            nbaProb = self.nbaTS.shape[0] / self.countTS
-            nflProb = self.nflTS.shape[0] / self.countTS
-            OverwatchProb = self.OverwatchTS.shape[0] / self.countTS
-            soccerProb = self.soccerTS.shape[0] / self.countTS
-            treesProb = self.treesTS.shape[0] / self.countTS
-            worldnewsProb = self.worldnewsTS.shape[0] / self.countTS
-            wowProb= self.wowTS.shape[0] / self.countTS
+            cpList = [] # class probability list (prediction probability that comment could be from the specific community)
+            for k in range(numClasses):
+                featureLikelihood = 0
+                for j in range(numFeatures):
+                    featureLikelihood += x[j] * np.log(self.thetaJY[k][j]) + (1 - x[j]) * np.log(1 - self.thetaJY[k][j])
+                cpList[k] = featureLikelihood + np.log(self.thetaY[k])
+            predictList[i] = cpList.index(max(cpList))
 
-            cpList = []
-
-            for j  in range(numCols):
-                set = self.animeTS[np.where(self.animeTS[:,j] == 1)]
-                animeProb = animeProb * ((set.shape[0] + 1) / (self.animeTS.shape[0] + 2))
-
-                set = self.AskRedditTS[np.where(self.AskRedditTS[:, j] == 1)]
-                AskRedditProb = AskRedditProb * ((set.shape[0] + 1) / (self.AskRedditTS.shape[0] + 2))
-
-                set = self.baseballTS[np.where(self.baseballTS[:, j] == 1)]
-                baseballProb = baseballProb * ((set.shape[0] + 1) / (self.baseballTS.shape[0] + 2))
-
-                set = self.canadaTS[np.where(self.canadaTS[:, j] == 1)]
-                canadaProb = canadaProb * ((set.shape[0] + 1) / (self.canadaTS.shape[0] + 2))
-
-                set = self.conspiracyTS[np.where(self.conspiracyTS[:, j] == 1)]
-                conspiracyProb = conspiracyProb * ((set.shape[0] + 1) / (self.conspiracyTS.shape[0] + 2))
-
-                set = self.europeTS[np.where(self.europeTS[:, j] == 1)]
-                europeProb = europeProb * ((set.shape[0] + 1) / (self.europeTS.shape[0] + 2))
-
-                set = self.funnyTS[np.where(self.funnyTS[:, j] == 1)]
-                funnyProb = funnyProb * ((set.shape[0] + 1) / (self.funnyTS.shape[0] + 2))
-
-                set = self.gameofthronesTS[np.where(self.animeTS[:, j] == 1)]
-                gameofthronesProb = gameofthronesProb * ((set.shape[0] + 1) / (self.gameofthronesTS.shape[0] + 2))
-
-                set = self.GlobalOffensiveTS[np.where(self.GlobalOffensiveTS[:, j] == 1)]
-                GlobalOffensiveProb = GlobalOffensiveProb * ((set.shape[0] + 1) / (self.GlobalOffensiveTS.shape[0] + 2))
-
-                set = self.hockeyTS[np.where(self.animeTS[:, j] == 1)]
-                hockeyProb = hockeyProb * ((set.shape[0] + 1) / (self.hockeyTS.shape[0] + 2))
-
-                set = self.leagueoflegnedsTS[np.where(self.leagueoflegnedsTS[:, j] == 1)]
-                leagueoflegendsProb = leagueoflegendsProb * ((set.shape[0] + 1) / (self.leagueoflegnedsTS.shape[0] + 2))
-
-                set = self.moviesTS[np.where(self.moviesTS[:, j] == 1)]
-                moviesProb = moviesProb * ((set.shape[0] + 1) / (self.moviesTS.shape[0] + 2))
-
-                set = self.MusicTS[np.where(self.MusicTS[:, j] == 1)]
-                MusicProb = MusicProb * ((set.shape[0] + 1) / (self.MusicTS.shape[0] + 2))
-
-                set = self.nbaTS[np.where(self.nbaTS[:, j] == 1)]
-                nbaProb = nbaProb * ((set.shape[0] + 1) / (self.nbaTS.shape[0] + 2))
-
-                set = self.nflTS[np.where(self.nflTS[:, j] == 1)]
-                nflProb = nflProb * ((set.shape[0] + 1) / (self.nflTS.shape[0] + 2))
-
-                set = self.OverwatchTS[np.where(self.OverwatchTS[:, j] == 1)]
-                OverwatchProb = OverwatchProb * ((set.shape[0] + 1) / (self.OverwatchTS.shape[0] + 2))
-
-                set = self.soccerTS[np.where(self.soccerTS[:, j] == 1)]
-                soccerProb = soccerProb * ((set.shape[0] + 1) / (self.soccerTS.shape[0] + 2))
-
-                set = self.treesTS[np.where(self.treesTS[:, j] == 1)]
-                treesProb = treesProb * ((set.shape[0] + 1) / (self.treesTS.shape[0] + 2))
-
-                set = self.worldnewsTS[np.where(self.worldnewsTS[:, j] == 1)]
-                worldnewsProb = worldnewsProb * ((set.shape[0] + 1) / (self.worldnewsTS.shape[0] + 2))
-
-                set = self.wowTS[np.where(self.wowTS[:, j] == 1)]
-                wowProb = wowProb * ((set.shape[0] + 1) / (self.wowTS.shape[0] + 2))
-
-            cpList.append(animeProb)
-            cpList.append(AskRedditProb)
-            cpList.append(baseballProb)
-            cpList.append(canadaProb)
-            cpList.append(conspiracyProb)
-            cpList.append(europeProb)
-            cpList.append(funnyProb)
-            cpList.append(gameofthronesProb)
-            cpList.append(GlobalOffensiveProb)
-            cpList.append(hockeyProb)
-            cpList.append(leagueoflegendsProb)
-            cpList.append(moviesProb)
-            cpList.append(MusicProb)
-            cpList.append(nbaProb)
-            cpList.append(nflProb)
-            cpList.append(OverwatchProb)
-            cpList.append(soccerProb)
-            cpList.append(treesProb)
-            cpList.append(worldnewsProb)
-            cpList.append(wowProb)
-
-            maxProbClass = cpList.index(max(cpList))  # highest probability belonging to class index (eg. anime = 0, AskReddit = 1, ..)
-            predictList.append(maxProbClass) # add the index of the highest probabilty class
-
-        return predictList
+        # this is for converting to numpy (column) array/vector
+        return np.asarray(predictList).transpose()
 
 def main():
 
@@ -182,7 +153,6 @@ def main():
     print(trainSet[0][0])
     print(trainSet[0][1])
     #'sets\\reddit_train.csv'
-
 
 if __name__ == '__main__':
     main()
