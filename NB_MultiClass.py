8c406a550e277bbf6417e08d2f61eb38a3661fa5
Junha Park <junha.park@mail.mcgill.ca>
2019-10-15 16:15:12 -0400

    included countvectorizer & excluded normalizing data set for bernoulli part of NB

--- NB_MultiClass.py
+++ NB_MultiClass.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.feature_selection import SelectPercentile
 from sklearn.preprocessing import normalize
 
@@ -12,124 +12,147 @@ class naiveBayes:
     thetaJY = {}
     thetaY = []
 
+    predictionClass = {
+        0:"anime",
+        1:"AskReddit",
+        2:"baseball",
+        3:"canada",
+        4:"conspiracy",
+        5:"europe",
+        6:"funny",
+        7:"gameofthrones",
+        8:"GlobalOffensive",
+        9:"hockey",
+        10:"leagueoflegends",
+        11:"movies",
+        12:"Music",
+        13:"nba",
+        14:"nfl",
+        15:"Overwatch",
+        16:"soccer",
+        17:"trees",
+        18:"worldnews",
+        19:"wow"
+    }
+
     def __init__(self):
         pass
 
     def fit(self, trainX, trainY):
 
         countTS = trainX.shape[0]  # number of training examples
-        askRedditTS = trainX[np.where(trainY[:] == "anime")]
-        animeTS = trainX[np.where(trainY[:] == "AskReddit")]
-        baseballTS = trainX[np.where(trainY[:] == "baseball")]
-        canadaTS = trainX[np.where(trainY[:] == "canada")]
-        conspiracyTS = trainX[np.where(trainY[:] == "conspiracy")]
-        europeTS = trainX[np.where(trainY[:] == "europe")]
-        funnyTS = trainX[np.where(trainY[:] == "funny")]
-        gameofthronesTS = trainX[np.where(trainY[:] == "gameofthrones")]
-        globalOffensiveTS = trainX[np.where(trainY[:] == "GlobalOffensive")]
-        hockeyTS = trainX[np.where(trainY[:] == "hockey")]
-        leagueoflegnedsTS = trainX[np.where(trainY[:] == "leagueoflegends")]
-        moviesTS = trainX[np.where(trainY[:] == "movies")]
-        musicTS = trainX[np.where(trainY[:] == "Music")]
-        nbaTS = trainX[np.where(trainY[:] == "nba")]
-        nflTS = trainX[np.where(trainY[:] == "nfl")]
-        overwatchTS = trainX[np.where(trainY[:] == "Overwatch")]
-        soccerTS = trainX[np.where(trainY[:] == "soccer")]
-        treesTS = trainX[np.where(trainY[:] == "trees")]
-        worldnewsTS = trainX[np.where(trainY[:] == "worldnews")]
-        wowTS = trainX[np.where(trainY[:] == "wow")]
+        animeTS = trainX[np.where(trainY[:] == self.predictionClass[0])]
+        #a = animeTS[0][6006]
 
-        self.thetaY[0] = animeTS.shape[0] / countTS
-        self.thetaY[1] = askRedditTS.shape[0] / countTS
-        self.thetaY[2] = baseballTS.shape[0] / countTS
-        self.thetaY[3] = canadaTS.shape[0] / countTS
-        self.thetaY[4] = conspiracyTS.shape[0] / countTS
-        self.thetaY[5] = europeTS.shape[0] / countTS
-        self.thetaY[6] = funnyTS.shape[0] / countTS
-        self.thetaY[7] = gameofthronesTS.shape[0] / countTS
-        self.thetaY[8] = globalOffensiveTS.shape[0] / countTS
-        self.thetaY[9] = hockeyTS.shape[0] / countTS
-        self.thetaY[10] = leagueoflegnedsTS.shape[0] / countTS
-        self.thetaY[11] = moviesTS.shape[0] / countTS
-        self.thetaY[12] = musicTS.shape[0] / countTS
-        self.thetaY[13] = nbaTS.shape[0] / countTS
-        self.thetaY[14] = nflTS.shape[0] / countTS
-        self.thetaY[15] = overwatchTS.shape[0] / countTS
-        self.thetaY[16] = soccerTS.shape[0] / countTS
-        self.thetaY[17] = treesTS.shape[0] / countTS
-        self.thetaY[18] = worldnewsTS.shape[0] / countTS
-        self.thetaY[19] = wowTS.shape[0] / countTS
+        askRedditTS = trainX[np.where(trainY[:] == self.predictionClass[1])]
+        baseballTS = trainX[np.where(trainY[:] == self.predictionClass[2])]
+        canadaTS = trainX[np.where(trainY[:] == self.predictionClass[3])]
+        conspiracyTS = trainX[np.where(trainY[:] == self.predictionClass[4])]
+        europeTS = trainX[np.where(trainY[:] == self.predictionClass[5])]
+        funnyTS = trainX[np.where(trainY[:] == self.predictionClass[6])]
+        gameofthronesTS = trainX[np.where(trainY[:] == self.predictionClass[7])]
+        globalOffensiveTS = trainX[np.where(trainY[:] == self.predictionClass[8])]
+        hockeyTS = trainX[np.where(trainY[:] == self.predictionClass[9])]
+        leagueoflegnedsTS = trainX[np.where(trainY[:] == self.predictionClass[10])]
+        moviesTS = trainX[np.where(trainY[:] == self.predictionClass[11])]
+        musicTS = trainX[np.where(trainY[:] == self.predictionClass[12])]
+        nbaTS = trainX[np.where(trainY[:] == self.predictionClass[13])]
+        nflTS = trainX[np.where(trainY[:] == self.predictionClass[14])]
+        overwatchTS = trainX[np.where(trainY[:] == self.predictionClass[15])]
+        soccerTS = trainX[np.where(trainY[:] == self.predictionClass[16])]
+        treesTS = trainX[np.where(trainY[:] == self.predictionClass[17])]
+        worldnewsTS = trainX[np.where(trainY[:] == self.predictionClass[18])]
+        wowTS = trainX[np.where(trainY[:] == self.predictionClass[19])]
+
+        self.thetaY.append(animeTS.shape[0] / countTS)
+        self.thetaY.append(askRedditTS.shape[0] / countTS)
+        self.thetaY.append(baseballTS.shape[0] / countTS)
+        self.thetaY.append(canadaTS.shape[0] / countTS)
+        self.thetaY.append(conspiracyTS.shape[0] / countTS)
+        self.thetaY.append(europeTS.shape[0] / countTS)
+        self.thetaY.append(funnyTS.shape[0] / countTS)
+        self.thetaY.append(gameofthronesTS.shape[0] / countTS)
+        self.thetaY.append(globalOffensiveTS.shape[0] / countTS)
+        self.thetaY.append(hockeyTS.shape[0] / countTS)
+        self.thetaY.append(leagueoflegnedsTS.shape[0] / countTS)
+        self.thetaY.append(moviesTS.shape[0] / countTS)
+        self.thetaY.append(musicTS.shape[0] / countTS)
+        self.thetaY.append(nbaTS.shape[0] / countTS)
+        self.thetaY.append(nflTS.shape[0] / countTS)
+        self.thetaY.append(overwatchTS.shape[0] / countTS)
+        self.thetaY.append(soccerTS.shape[0] / countTS)
+        self.thetaY.append(treesTS.shape[0] / countTS)
+        self.thetaY.append(worldnewsTS.shape[0] / countTS)
+        self.thetaY.append(wowTS.shape[0] / countTS)
 
         numCols = trainX.shape[1]
 
         for j in range(numCols):
             self.thetaJY[j] = {}
 
-            set = animeTS[np.where(animeTS[:, j] == 1)]
-            self.thetaJY[j][0] = (set.shape[0] + 1) / (animeTS.shape[0] + 2)
+            count = animeTS[:, j].count_nonzero()
+            self.thetaJY[j][0] = (count + 1) / (animeTS.shape[0] + 2)
 
-            set = askRedditTS[np.where(askRedditTS[:, j] == 1)]
-            self.thetaJY[j][1] = (set.shape[0] + 1) / (askRedditTS.shape[0] + 2)
+            count = askRedditTS[:, j].count_nonzero()
+            self.thetaJY[j][1] = (count + 1) / (askRedditTS.shape[0] + 2)
 
-            set = baseballTS[np.where(baseballTS[:, j] == 1)]
-            self.thetaJY[j][2] = (set.shape[0] + 1) / (baseballTS.shape[0] + 2)
+            count = baseballTS[:, j].count_nonzero()
+            self.thetaJY[j][2] = (count + 1) / (baseballTS.shape[0] + 2)
 
-            set = canadaTS[np.where(canadaTS[:, j] == 1)]
-            self.thetaJY[j][3] = (set.shape[0] + 1) / (canadaTS.shape[0] + 2)
+            count = canadaTS[:, j].count_nonzero()
+            self.thetaJY[j][3] = (count + 1) / (canadaTS.shape[0] + 2)
 
-            set = conspiracyTS[np.where(conspiracyTS[:, j] == 1)]
-            self.thetaJY[j][4] = (set.shape[0] + 1) / (conspiracyTS.shape[0] + 2)
+            count = conspiracyTS[:, j].count_nonzero()
+            self.thetaJY[j][4] = (count + 1) / (conspiracyTS.shape[0] + 2)
 
-            set = europeTS[np.where(europeTS[:, j] == 1)]
-            self.thetaJY[j][5] = (set.shape[0] + 1) / (europeTS.shape[0] + 2)
+            count = europeTS[:, j].count_nonzero()
+            self.thetaJY[j][5] = (count + 1) / (europeTS.shape[0] + 2)
 
-            set = funnyTS[np.where(funnyTS[:, j] == 1)]
-            self.thetaJY[j][6] = (set.shape[0] + 1) / (funnyTS.shape[0] + 2)
+            count = funnyTS[:, j].count_nonzero()
+            self.thetaJY[j][6] = (count + 1) / (funnyTS.shape[0] + 2)
 
-            set = gameofthronesTS[np.where(gameofthronesTS[:, j] == 1)]
-            self.thetaJY[j][7] = (set.shape[0] + 1) / (gameofthronesTS.shape[0] + 2)
+            count = gameofthronesTS[:, j].count_nonzero()
+            self.thetaJY[j][7] = (count + 1) / (gameofthronesTS.shape[0] + 2)
 
-            set = globalOffensiveTS[np.where(globalOffensiveTS[:, j] == 1)]
-            self.thetaJY[j][8] = (set.shape[0] + 1) / (globalOffensiveTS.shape[0] + 2)
+            count = globalOffensiveTS[:, j].count_nonzero()
+            self.thetaJY[j][8] = (count + 1) / (globalOffensiveTS.shape[0] + 2)
 
-            set = hockeyTS[np.where(hockeyTS[:, j] == 1)]
-            self.thetaJY[j][9] = (set.shape[0] + 1) / (hockeyTS.shape[0] + 2)
+            count = hockeyTS[:, j].count_nonzero()
+            self.thetaJY[j][9] = (count + 1) / (hockeyTS.shape[0] + 2)
 
-            set = leagueoflegnedsTS[np.where(leagueoflegnedsTS[:, j] == 1)]
-            self.thetaJY[j][10] = (set.shape[0] + 1) / (leagueoflegnedsTS.shape[0] + 2)
+            count = leagueoflegnedsTS[:, j].count_nonzero()
+            self.thetaJY[j][10] = (count + 1) / (leagueoflegnedsTS.shape[0] + 2)
 
-            set = moviesTS[np.where(moviesTS[:, j] == 1)]
-            self.thetaJY[j][11] = (set.shape[0] + 1) / (moviesTS.shape[0] + 2)
+            count = moviesTS[:, j].count_nonzero()
+            self.thetaJY[j][11] = (count + 1) / (moviesTS.shape[0] + 2)
 
-            set = musicTS[np.where(musicTS[:, j] == 1)]
-            self.thetaJY[j][12] = (set.shape[0] + 1) / (musicTS.shape[0] + 2)
+            count = musicTS[:, j].count_nonzero()
+            self.thetaJY[j][12] = (count + 1) / (musicTS.shape[0] + 2)
 
-            set = nbaTS[np.where(nbaTS[:, j] == 1)]
-            self.thetaJY[j][13] = (set.shape[0] + 1) / (nbaTS.shape[0] + 2)
+            count = nbaTS[:, j].count_nonzero()
+            self.thetaJY[j][13] = (count + 1) / (nbaTS.shape[0] + 2)
 
-            set = nflTS[np.where(nflTS[:, j] == 1)]
-            self.thetaJY[j][14] = (set.shape[0] + 1) / (nflTS.shape[0] + 2)
+            count = nflTS[:, j].count_nonzero()
+            self.thetaJY[j][14] = (count + 1) / (nflTS.shape[0] + 2)
 
-            set = overwatchTS[np.where(overwatchTS[:, j] == 1)]
-            self.thetaJY[j][15] = (set.shape[0] + 1) / (overwatchTS.shape[0] + 2)
+            count = overwatchTS[:, j].count_nonzero()
+            self.thetaJY[j][15] = (count + 1) / (overwatchTS.shape[0] + 2)
 
-            set = soccerTS[np.where(soccerTS[:, j] == 1)]
-            self.thetaJY[j][16] = (set.shape[0] + 1) / (soccerTS.shape[0] + 2)
+            count = soccerTS[:, j].count_nonzero()
+            self.thetaJY[j][16] = (count + 1) / (soccerTS.shape[0] + 2)
 
-            set = treesTS[np.where(treesTS[:, j] == 1)]
-            self.thetaJY[j][17] = (set.shape[0] + 1) / (treesTS.shape[0] + 2)
+            count = treesTS[:, j].count_nonzero()
+            self.thetaJY[j][17] = (count + 1) / (treesTS.shape[0] + 2)
 
-            set = worldnewsTS[np.where(askRedditTS[:, j] == 1)]
-            self.thetaJY[j][18] = (set.shape[0] + 1) / (askRedditTS.shape[0] + 2)
+            count = worldnewsTS[:, j].count_nonzero()
+            self.thetaJY[j][18] = (count + 1) / (worldnewsTS.shape[0] + 2)
 
-            set = wowTS[np.where(wowTS[:, j] == 1)]
-            self.thetaJY[j][19] = (set.shape[0] + 1) / (wowTS.shape[0] + 2)
-
-
+            count = wowTS[:, j].count_nonzero()
+            self.thetaJY[j][19] = (count + 1) / (wowTS.shape[0] + 2)
 
     def predict(self, testSet):
         numRows = testSet.shape[0] #number of testset
-        numClasses = 20
+        numClasses = len(self.predictionClass)
         numFeatures = testSet.shape[1]
         predictList = [] #collection of prediction class for each test example
         for i in range(numRows):
@@ -138,12 +161,16 @@ class naiveBayes:
             for k in range(numClasses):
                 featureLikelihood = 0
                 for j in range(numFeatures):
-                    featureLikelihood += x[j] * np.log(self.thetaJY[k][j]) + (1 - x[j]) * np.log(1 - self.thetaJY[k][j])
-                cpList[k] = featureLikelihood + np.log(self.thetaY[k])
-            predictList[i] = cpList.index(max(cpList))
-
+                    if i in x.indices: #check if test sample's feature is 1
+                        featureLikelihood += np.log(self.thetaJY[j][k])
+                    else:
+                        featureLikelihood += np.log(1 - self.thetaJY[j][k])
+                cpList.append(featureLikelihood + np.log(self.thetaY[k]))
+            #add the right prediction
+            index = cpList.index(max(cpList))
+            predictList.append(self.predictionClass[index])
         # this is for converting to numpy (column) array/vector
-        return np.asarray(predictList).transpose()
+        return np.array(predictList)
 
 def main():
 
@@ -161,19 +188,15 @@ def main():
     test_x = parsed_Test['comments']
 
     # tf*idf vectorizer
-    tf_idf_vectorizer = TfidfVectorizer(analyzer='word')
-    vectors_train_idf = tf_idf_vectorizer.fit_transform(train_x)
-    vectors_test_idf = tf_idf_vectorizer.transform(test_x)
+    vectorizer = CountVectorizer(binary = True)
+    vectors_train_idf = vectorizer.fit_transform(train_x)
+    vectors_test_idf = vectorizer.transform(test_x)
     # normalization
-    vectors_train_idf_normalized = normalize(vectors_train_idf)
-    vectors_test_idf_normalized = normalize(vectors_test_idf)
-    # pre-processing / selector
-    # sel = VarianceThreshold(threshold=(0.8))
-    # vectors_train_idf_selected = sel.fit(vectors_train_idf_normalized)
-    # vectors_test_idf_selected = sel.fit(vectors_test_idf_normalized)
-    selector = SelectPercentile(percentile=25)
-    vectors_train_idf_selected = selector.fit_transform(vectors_train_idf_normalized, train_y)
-    vectors_test_idf_selected = selector.transform(vectors_test_idf_normalized)
+    #vectors_train_idf_normalized = normalize(vectors_train_idf)
+    #vectors_test_idf_normalized = normalize(vectors_test_idf)
+    selector = SelectPercentile(percentile=10)
+    vectors_train_idf_selected = selector.fit_transform(vectors_train_idf, train_y)
+    vectors_test_idf_selected = selector.transform(vectors_test_idf)
 
     nb = naiveBayes()
     nb.fit(vectors_train_idf_selected, train_y)
