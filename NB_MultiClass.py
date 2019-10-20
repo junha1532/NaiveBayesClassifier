5f9b88cdfc3716fcaee1ed9bd0dc0437fe32b9f9
Junha Park <junha.park@mail.mcgill.ca>
2019-10-20 12:19:33 -0400

    implemented k-fold with bernoulli naive bayes / tested with k-fold different feature extraction techniques

--- NB_MultiClass.py
+++ NB_MultiClass.py
@@ -2,11 +2,20 @@ import numpy as np
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
-from scipy.sparse import csr_matrix
+from nltk import word_tokenize
+from scipy.sparse import csr_matrix, vstack
 from sklearn import model_selection, metrics
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.feature_selection import SelectPercentile
 from sklearn.preprocessing import normalize, binarize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
+stop_words = set(stopwords.words('english'))
 
 
 class naiveBayes:
@@ -69,6 +78,41 @@ class naiveBayes:
             featureLikelihood = np.sum(featureLikelihood)
             cpList.append(featureLikelihood + np.log(self.thetaY[k]))
         return cpList
+def kfold(folds, model, train_x, train_y):
+    foldSize = train_x.shape[0] // folds
+    fold_accuracy = []
+    for i in range(folds):
+        start = i * foldSize
+        end = (i + 1) * foldSize - 1
+        testSubset = train_x[start:end]
+        yTestSubset = train_y[start:end]
+        if i == folds - 1:
+            trainSubset = train_x[0:start]
+            yTrainSubset = train_y[0:start]
+        elif i ==0:
+            trainSubset = train_x[end:]
+            yTrainSubset = train_y[end:]
+        else:
+            trainSubset = vstack((train_x[0:start], train_x[end:]))
+            yTrainSubset = pd.concat([train_y[0:start], train_y[end:]], ignore_index=True)
+
+        model.fit(trainSubset, yTrainSubset)
+
+        predictions = model.predict(testSubset)
+        acc = metrics.accuracy_score(predictions, yTestSubset)
+        # acc = evaluate_acc(model, testSubset, yTestSubset)
+        fold_accuracy.append(acc)
+    return fold_accuracy
+# def evaluate_acc(model, testSet, y):
+#     accuracy = 0
+#     numRows = testSet.shape[0]
+#     for i in range(numRows):
+#         yi = y[i]
+#         x = np.asmatrix(testSet[i]).transpose()
+#         prediction = model.predict(x)
+#         if prediction == yi:
+#             accuracy += 1.0
+#     return accuracy / numRows
 
 def main():
 
@@ -78,15 +122,14 @@ def main():
     # load dataset into a panda dataframe
     parsed_Set = pd.read_csv(training_setfile, header=0, sep=',')
     parsed_Test = pd.read_csv(testing_setfile, header=0, sep=',')
-    train_x, test_x, train_y, test_y = model_selection.train_test_split(
-             parsed_Set['comments'], parsed_Set['subreddits'], train_size=0.8, test_size=0.2)
-
-    # train_x = parsed_Set['comments']
-    # train_y = parsed_Set['subreddits']
-    # test_x = parsed_Test['comments']
+    # train_x, test_x, train_y, test_y = model_selection.train_test_split(
+    #          parsed_Set['comments'], parsed_Set['subreddits'], train_size=0.8, test_size=0.2)
+    train_x = parsed_Set['comments']
+    train_y = parsed_Set['subreddits']
+    test_x = parsed_Test['comments']
 
     # tf*idf vectorizer
-    vectorizer = TfidfVectorizer(binary= True, use_idf=False, norm=None)
+    vectorizer = TfidfVectorizer(binary= True, use_idf=False, norm=None, tokenizer=LemmaTokenizer())
     vectors_train_idf = vectorizer.fit_transform(train_x)
     vectors_test_idf = vectorizer.transform(test_x)
     # normalization
@@ -96,11 +139,15 @@ def main():
     vectors_train_idf_selected = selector.fit_transform(vectors_train_idf, train_y)
     vectors_test_idf_selected = selector.transform(vectors_test_idf)
 
-    nb = naiveBayes(alpha = 1.0)
-    nb.fit(vectors_train_idf_selected, train_y)
-    predictions = nb.predict(vectors_test_idf_selected)
-    acc = metrics.accuracy_score(predictions, test_y)
-    print("Naive Bayes prediction = ", acc)
+    nb = naiveBayes(alpha=1.0)
+    kfold_avg = kfold(5, nb, vectors_train_idf_selected, train_y)
+    print("Model k-fold accuracies : ", kfold_avg)
+
+    # nb.fit(vectors_train_idf_selected, train_y)
+    # predictions = nb.predict(vectors_test_idf_selected)
+    # acc = metrics.accuracy_score(predictions, test_y)
+    # print("naive bayes multi with idf: ", acc)
+
     # pd.DataFrame(predictions).to_csv("sets\\prediction.csv", header = ["Category"], index_label= "Id")
     # print("done!")
 
